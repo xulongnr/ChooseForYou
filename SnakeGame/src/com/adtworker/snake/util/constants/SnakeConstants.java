0060a3d76cf625b2d1193ce6d2b37447ab803bea
xulongnr <xulongnr@gmail.com>
2012-07-24 00:09:13 +0800

    Added suizong

---
+++ SnakeGame/src/com/adtworker/snake/util/constants/SnakeConstants.java
@@ -0,0 +1,24 @@
+package com.adtworker.snake.util.constants;
+
+/**
+ * (c) 2010 Nicolas Gramlich 
+ * (c) 2011 Zynga Inc.
+ * 
+ * @author Nicolas Gramlich
+ * @since 17:42:10 - 09.07.2010
+ */
+public interface SnakeConstants {
+	// ===========================================================
+	// Final Fields
+	// ===========================================================
+	
+	public static final int CELLS_HORIZONTAL = 32;
+	public static final int CELLS_VERTICAL = 18;
+	
+	public static final int CELL_WIDTH = 16;
+	public static final int CELL_HEIGHT = CELL_WIDTH;
+
+	// ===========================================================
+	// Methods
+	// ===========================================================
+}
